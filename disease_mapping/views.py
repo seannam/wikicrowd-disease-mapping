684669b2790fa201b30f6098bea7963cae490fd0
Sean Nam <namsangwoo1@gmail.com>
2018-04-13 14:16:28 -0700

    Create disease queue table

--- disease_mapping/views.py
+++ disease_mapping/views.py
@@ -3,5 +3,31 @@ from django.http import HttpResponse
 
 
 def index(request):
-    context = {}
+    diseases = [
+        {
+            "name": "Systemic Lupus Erythematosus",
+            "DOID": "DOID9034",
+            "match_status": True,
+        },
+        {
+            "name": "Systemic Lupine Ethemosuss",
+            "DOID": "DOID1234",
+            "match_status": False,
+        },
+        {
+            "name": "Lupine Erythrisis",
+            "DOID": "DOID4234",
+            "match_status": False,
+        },
+        {
+            "name": "Lupinedid Erythrsmousis",
+            "DOID": "DOID8234",
+            "match_status": False,
+        },
+    ]
+
+    context = {
+        'diseases': diseases,
+    }
+
     return render(request, 'disease_mapping/index.html', context)
